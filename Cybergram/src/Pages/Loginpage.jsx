13e2002ac8f378e2c4c74233328d708313d3271f
SebasEsc6 <110470320+SebasEsc6@users.noreply.github.com>
2023-03-01 18:04:31 -0500

    Se añaden los componentes de las paginas de login y register de la pagina web

---
+++ Cybergram/src/Pages/Loginpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Gohome from '../Components/Shared/Gohome'
+
+function Loginpage() {
+  return (
+    <div>
+        <Gohome />
+        Loginpage
+    </div>
+  )
+}
+
+export default Loginpage
---
+++ Cybergram/src/Pages/Registerpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Gohome from '../Components/Shared/Gohome'
+
+function Registerpage() {
+  return (
+    <div>
+        <Gohome />
+        Registerpage
+    </div>
+  )
+}
+
+export default Registerpage          
